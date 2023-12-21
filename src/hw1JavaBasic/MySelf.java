2a36a39c7d6d02aaf1737ae854a8949c532a024a
kafikhan786 <93836962+kafikhan786@users.noreply.github.com>
2023-12-21 18:38:59 -0500

    Java Basic

---
+++ src/hw1JavaBasic/MySelf.java
@@ -0,0 +1,12 @@
+package hw1JavaBasic;
+
+public class MySelf {
+
+	public static void main(String[] args) {
+
+//		err used to see the error in red color outcome in console 
+		System.err.println("Buffalo \nNY");
+
+	}
+
+}
---
+++ src/hw3JavaVariables/AboutMe.java
@@ -0,0 +1,41 @@
+package hw3JavaVariables;
+
+public class AboutMe {
+
+	public String customerDetail; // Variable declared
+
+	public String customerName = "Adam";
+
+	public byte cusAge = 110; // byte runs between -128 to 127
+	public short cusCard = 1234;
+	public int cusPhone = 646549212;
+	public long cusAccNumber = 123456789l; // use l at the end
+
+	public float cusHeight = 5.5f; // use f at the end
+	public double cusRewards = 223.5647;
+
+	public char CusGender = 'M';
+	public boolean cusMembership = true;
+
+	// Constructor declared
+
+	public AboutMe() {
+		System.out.println("Customer Information: ");
+	}
+
+	
+	/*
+	 * public static void main(String[] args) { AboutMe aboutMe = new AboutMe();
+	 * System.out.println(aboutMe.customerName);
+	 * System.out.println(aboutMe.cusCard); System.out.println(aboutMe.cusPhone);
+	 * System.out.println(aboutMe.cusAccNumber);
+	 * System.out.println(aboutMe.cusHeight);
+	 * System.out.println(aboutMe.cusRewards);
+	 * System.out.println(aboutMe.CusGender);
+	 * System.out.println(aboutMe.cusMembership);
+	 * 
+	 * }
+	 */
+	 
+
+}

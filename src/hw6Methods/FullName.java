789e138e514ac0ec7ddefdbc1eb9e98b444111fc
kafikhan786 <kafikhan786>
2023-12-30 14:27:08 -0500

    HW6

---
+++ src/hw6Methods/FullName.java
@@ -0,0 +1,28 @@
+package hw6Methods;
+
+public class FullName {
+	
+	public String fullName(String fName, String lName) {
+		
+		String firstAndLastName = fName + lName;
+		
+		System.out.println("Family Member: " +fName + lName);
+		return firstAndLastName;
+	}
+	
+	public static void main (String[]args) {
+		FullName fullName= new FullName();
+		fullName.fullName("Rakib", " Khan");
+		fullName.fullName("A", " Khan");
+		fullName.fullName("B", " Khan");
+		fullName.fullName("C", " Khan");
+		fullName.fullName("D", " Khan");
+		fullName.fullName("E", " Khan");
+	}
+	
+	
+	
+	
+	
+
+}

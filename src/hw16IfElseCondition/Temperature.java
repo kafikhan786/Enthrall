a287c251542113d4f5dfb918fcf50def998c94f2
kafikhan786 <kafikhan786>
2024-01-23 22:19:34 -0500

    hw

---
+++ src/hw16IfElseCondition/Temperature.java
@@ -0,0 +1,32 @@
+package hw16IfElseCondition;
+
+import java.util.Scanner;
+
+public class Temperature {
+
+	public static void main(String[] args) {
+		System.out.println("Today's Weather");
+
+		Scanner scanner = new Scanner(System.in);
+		int todaysTemperature = scanner.nextInt();
+
+		if (todaysTemperature < 32) {
+			System.out.println(todaysTemperature + " Freezing");
+
+		} else if (todaysTemperature < 55) {
+			System.out.println(todaysTemperature + " Pleasant");
+
+		} else if (todaysTemperature < 73) {
+			System.out.println(todaysTemperature + " Getting Warmer");
+
+		} else if (todaysTemperature > 101) {
+			System.out.println(todaysTemperature + " Very Hot");
+
+		} else {
+			System.out.println(todaysTemperature + " Please put a valid temperature");
+		}
+
+		scanner.close();
+
+	}
+}
